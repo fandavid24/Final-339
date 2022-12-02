e74d59ea9e0c6b6cb88634ba8e246c321eb01584
fandavid <fandavid@umich.edu>
2022-12-02 15:01:36 -0500

    fixed quotes, added ye

--- script2.js
+++ script2.js
@@ -5,26 +5,41 @@ createApp({
         return {
             qT: 'quoteText goes here',
             qA: '',
+            kanye: "Ball so hard that shit cray"
         }
     },
     methods: {
         async getQuote() {
-            // const res = fetch('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en', { mode: 'no-cors' })
-            // console.log(res)
-            $.ajax({
-                jsonp: "jsonp",
-                dataType: "jsonp",
-                url: "http://api.forismatic.com/api/1.0/",
-                contentType: "application/jsonp",
-                data: {
-                    lang: "en",
-                    method: "getQuote",
-                    format: 'jsonp'
-                },
-                success: function (data) {
-                    document.getElementById("quoteHere").innerHTML = "<q>" + data.quoteText + "</q>" + "<br> -" + data.quoteAuthor
-                }
-            })
+            // const res = await fetch('https://itunes.apple.com/search?attribute=allArtistTerm&term=' + this.term)
+            // const { results } = await res.json()
+            const response = await fetch("https://api.goprogram.ai/inspiration");
+            var data = await response.json();
+            console.log(data);
+            this.qT = data.quote
+            this.qA = data.author
+            const res = await fetch("https://api.kanye.rest");
+            var ye = await res.json();
+            console.log(ye);
+            this.kanye = ye.quote
+
+
+            // $.ajax({
+            //     jsonp: "jsonp",
+            //     dataType: "jsonp",
+            //     url: "http://api.forismatic.com/api/1.0/",
+            //     contentType: "application/jsonp",
+            //     data: {
+            //         lang: "en",
+            //         method: "getQuote",
+            //         format: 'jsonp'
+            //     },
+            //     success: function (data) {
+            //         if (data.quoteAuthor == '') {
+            //             data.quoteAuthor = "Unkown"
+            //         }
+            //         document.getElementById("quoteHere").innerHTML = "<q>" + data.quoteText + "</q>" + "<br> -" + data.quoteAuthor
+            //     }
+            // })
         },
     },
     beforeMount() {
